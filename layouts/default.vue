dc3d5149396d484718515f5f5c10c1a9ed468e67
yamatoooooooo <miktoklimbu32@gmail.com>
2023-06-12 16:50:37 +0545

    nuxt page created

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <header>
+
+        </header>
+        <main>
+            <slot></slot>
+        </main>
+        <footer>
+
+        </footer>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <section class="hero py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>welcome to home page
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem quia, ipsa exercitationem odio harum provident! Tenetur quos eum necessitatibus ratione exercitationem error voluptate ut dicta voluptatum obcaecati aliquam inventore vitae, laboriosam earum, aspernatur ullam ad magnam ipsum excepturi corporis dolores asperiores deleniti labore consectetur. Deleniti quae numquam unde accusantium. Atque quo animi velit in culpa maiores inventore molestias possimus ex accusamus pariatur cum, quam repudiandae dolores saepe? Mollitia ipsam, pariatur quidem nemo impedit accusamus necessitatibus nihil odit, similique vitae nostrum ducimus eligendi! Iure aspernatur voluptate officiis doloremque accusantium nostrum blanditiis laudantium, itaque minima beatae voluptatum dignissimos optio! Non, itaque voluptatum!</p>
+                        </h1>
+                    </div>
+                </div> 
+            </div>
+        </section>
+    
+    <!-- card -->
+    <section class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="https://cdn.pixabay.com/photo/2023/05/30/15/43/koala-8028992_640.jpg" alt="">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, quas. Perferendis beatae nam in, distinctio non placeat, natus tenetur dolorum sunt est blanditiis officia ex?</p>
+        
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <img src="https://cdn.pixabay.com/photo/2023/05/30/15/43/koala-8028992_640.jpg" alt="">
+                            </div>
+                            <div class="col-md-6">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, possimus sapiente quo unde fugiat distinctio omnis culpa totam, tenetur earum consequuntur alias exercitationem doloribus commodi.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+.hero{
+    background-color: aqua;
+    text-align: center;
+    height: 300px;
+}
+
+.card{
+     border-radius: 10px;
+     overflow: hidden;
+     box-shadow: 5px 5px 5px 5px aqua;
+
+}
+</style>
